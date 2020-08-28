30c2fc7cdda00eadfe9ca8b1ef5b023d4fd107b4
anand100b <57651518+anand100b@users.noreply.github.com>
2020-08-28 22:47:50 +1000

    delete function added

--- components/Item.js
+++ components/Item.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text,View,TouchableOpacity,Image } from 'react-native';
 
-export const Item = (props) => {
-  return (
+import {itemStyles} from '../styles/Item';
+
+export const Item = ( props ) => {
+  return(
     <View style={itemStyles.item}>
-       <Text style={itemStyles.text}>{props.category}</Text>
-      <Text style={itemStyles.text}>{props.amount}</Text> 
+      <View style={itemStyles.row}>
+        <Text style={itemStyles.text}>{props.category}</Text>
+        <Text style={itemStyles.text}>{props.amount}</Text>
+      </View>
+      <TouchableOpacity onPress={ () => {props.delete( props.id ) } }>
+        <Image style={itemStyles.icon} source={require('../assets/trash-alt-solid.png')} />
+      </TouchableOpacity>
     </View>
   )
 }
-const itemStyles = StyleSheet.create({
-  item :{
-    padding:10,
-    display:'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  text:{
-    fontSize: 16,
-    color: 'black'
-  }
-})
